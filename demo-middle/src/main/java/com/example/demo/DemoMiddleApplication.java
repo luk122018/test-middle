469592785e33aabae1064718344cefd0c631055e
trabajo <trabajo@MSI>
2018-12-19 10:42:07 +0100

    initial commit

---
+++ demo-middle/src/main/java/com/example/demo/DemoMiddleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMiddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMiddleApplication.class, args);
+	}
+
+}
+
