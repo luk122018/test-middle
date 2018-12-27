ce2bdf9d64dc706fbdfd4ef447d8127b88877dae
trabajo <trabajo@MSI>
2018-12-27 17:11:05 +0100

    CORS

--- demo-middle/src/main/java/com/example/demo/DemoMiddleApplication.java
+++ demo-middle/src/main/java/com/example/demo/DemoMiddleApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class DemoMiddleApplication {
 	
 	@Autowired
 	DemoDTO demo;
-
+	 @CrossOrigin(origins = "http://localhost:8080")
 	 @RequestMapping(value = "/")
 	 @JsonFormat
 	   public DemoDTO success() {
